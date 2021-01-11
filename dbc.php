23d7e573086a227d8e7f8b1fecc486d0015c7a44
maa <abcdabcdacc@icloud.com>
2021-01-11 21:12:25 +0900

    変更

--- dbc.php
+++ dbc.php
@@ -1,18 +1,2 @@
 <?php
 
-function dbConnect() {
-    $dsn = 'mysql:host=localhost;dbname=blog_app;charset=utf8';
-    $user = 'blog_user';
-    $pass = '0506';
-    try {
-        $dbh = new \PDO($dsn,$user,$$pass,[
-        ]);
-    }catch (PDOExcption $e) {
-        echo '失敗'.$e->getMessage();
-        exit();
-    }
-    return $dbh;
-}
-function setCategoryName($category) {
-    
-}
